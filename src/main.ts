8b6c3cd31dda6990da25b0aa1490c3d2ca46884c
Jaden <hai.hoang@dabeeovina.com>
2025-04-03 10:30:16 +0700

    project initialization

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 8080);
+  await app.listen(port, () => {
+    console.log(`Server is Running on http://localhost:${port}`);
+  });
+}
+void bootstrap();
